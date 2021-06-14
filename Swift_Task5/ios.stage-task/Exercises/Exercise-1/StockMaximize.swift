41fa19b04d2f88cdefcbc472581a21fb58dae32c
djbottak@gmail.com <djbottak@gmail.com>
2021-06-14 20:11:16 +0300

    add final fix to project

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -4,24 +4,22 @@ class StockMaximize {
 
     
     func countProfit(prices: [Int]) -> Int {
-            var profit = 0
-            var max = 0
+            if prices.count == 0 || prices.count == 1 {
+                return 0
+            }
             
-            if prices.count > 0 {
-                for i in 1..<prices.count {
-                    if prices[i] >= prices[i-1] {
-                        for j in i..<prices.count {
-                            if prices[j] > prices[i] {
-                                max = prices[j]
-                            } else {
-                                max = prices[i]
-                            }
-                        }
-                        profit += max - prices[i-1]
-                    }
+            var profit = 0
+            var dayWhenCanSell = prices
+            
+            for i in 0..<prices.count - 1 {
+                dayWhenCanSell.remove(at: 0)
+                guard let maxPrice = dayWhenCanSell.sorted().last else {
+                    return 0
+                }
+                if prices[i] < maxPrice{
+                    profit += maxPrice - prices[i]
                 }
             }
-
             return profit
         }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,44 +17,52 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-            let foodMatrix = getMatrix(maxWeight, foods.count, foods)
-            let drinkMatrix = getMatrix(maxWeight, drinks.count, drinks)
-            
-            var maximumDistance = 0
-            for i in 1...maxWeight {
-                let currentFoodVariation = foodMatrix[foods.count][i]
-                let currentDrinkVariation = drinkMatrix[drinks.count][maxWeight-i] 
-                
-                if min(currentFoodVariation, currentDrinkVariation) > maximumDistance {
-                    maximumDistance = min(currentFoodVariation, currentDrinkVariation)
-                }
-            }
-            
-            return maximumDistance
-        }
-
-
-        func getMatrix (_ maxWeight: Int, _ numberOfElements: Int, _ array: [Supply]) -> [[Int]] {
-            var profits = [0]
-            var weights = [0]
-            for i in 0..<array.count {
-                profits.append(array[i].value)
-                weights.append(array[i].weight)
-            }
-            
-            var matrix = Array(repeating: Array(repeating: 0, count: maxWeight + 1), count: profits.count)
-            
-            for i in 1...numberOfElements {
-                for w in 1...maxWeight {
-                    if weights[i] <= w {
-                        matrix[i][w] = max((profits[i] + matrix[i-1][w-weights[i]]), matrix[i-1][w])
+            var goodsTrash: [[Int]] = Array(repeating: Array(repeating: 0, count: maxWeight + 1),
+                count: foods.count + 1)
+            var drinksTrash: [[Int]] = Array(repeating: Array(repeating: 0, count: maxWeight + 1), count: drinks.count + 1)
+            for i in 0..<foods.count {
+                let currentFood = foods[i]
+                for j in 1...maxWeight {
+                    if (((j - currentFood.weight > 0 && goodsTrash[i][j - currentFood.weight] > 0) ||
+                    j - currentFood.weight == 0) && goodsTrash[i][j - currentFood.weight] + currentFood.value > goodsTrash[i][j]) {
+                        goodsTrash[i + 1][j] = goodsTrash[i][j - currentFood.weight] + currentFood.value
                     } else {
-                        matrix[i][w] = matrix[i-1][w]
+                        goodsTrash[i + 1][j] = goodsTrash[i][j]
                     }
                 }
             }
-
             
-            return matrix
+            for i in 0..<drinks.count {
+                let currentDrink = drinks[i]
+                for j in 1...maxWeight {
+                    if (((j - currentDrink.weight > 0 && drinksTrash[i][j - currentDrink.weight] > 0) ||
+                    j - currentDrink.weight == 0) && drinksTrash[i][j - currentDrink.weight] + currentDrink.value > drinksTrash[i][j]) {
+                        drinksTrash[i + 1][j] = drinksTrash[i][j - currentDrink.weight] + currentDrink.value
+                    } else {drinksTrash[i + 1][j] = drinksTrash[i][j]}
+                }
+            }
+            for i in 1...maxWeight {
+                if (goodsTrash[foods.count][i] < goodsTrash[foods.count][i - 1]) {
+                    goodsTrash[foods.count][i] = goodsTrash[foods.count][i - 1]
+                }
+            }
+            for i in 1...maxWeight {
+                if (drinksTrash[drinks.count][i] < drinksTrash[drinks.count][i - 1]) {
+                    drinksTrash[drinks.count][i] = drinksTrash[drinks.count][i - 1]
+                }
+            }
+            var max = 0
+            for i in 0...maxWeight {
+                let currentFood = goodsTrash[foods.count][i]
+                let currentDrink = drinksTrash[drinks.count][maxWeight - i]
+                var result = currentFood
+                if (currentDrink < currentFood) {
+                    result = currentDrink
+                }
+                if (result > max) {
+                    max = result
+                }
+            }
+            return max
         }
 }
