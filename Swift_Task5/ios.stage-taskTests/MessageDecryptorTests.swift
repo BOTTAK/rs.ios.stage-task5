a69a05a260c831d3b3feeffbeb2847f0ef3425bd
djbottak@gmail.com <djbottak@gmail.com>
2021-06-14 20:13:12 +0300

    add fix to uitest

--- Swift_Task5/ios.stage-taskTests/MessageDecryptorTests.swift
+++ Swift_Task5/ios.stage-taskTests/MessageDecryptorTests.swift
@@ -14,56 +14,56 @@ class MessageDecryptorTests: XCTestCase {
     }
     
     func test1() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("secretmessage"), "secretmessage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("secretmessage"), "secretmessage")
     }
     
     func test2() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("[secretmessage]"), "secretmessage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("[secretmessage]"), "secretmessage")
     }
     
     func test3() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("0[secretmessage]"), "")
+        XCTAssertEqual(messageDecryptor.decryptMessage("0[secretmessage]"), "")
     }
     
     func test4() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("1[secretmessage]"), "secretmessage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("1[secretmessage]"), "secretmessage")
     }
     
     func test5() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("3[secret]2[message]"), "secretsecretsecretmessagemessage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("3[secret]2[message]"), "secretsecretsecretmessagemessage")
     }
     
     func test6() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("4[secret]me3[s]age"), "secretsecretsecretsecretmesssage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("4[secret]me3[s]age"), "secretsecretsecretsecretmesssage")
     }
     
     func test7() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("s4[e]cr3[e]tme2[ss]age"), "seeeecreeetmessssage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("s4[e]cr3[e]tme2[ss]age"), "seeeecreeetmessssage")
     }
     
     func test8() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("3[message2[secret]]"), "messagesecretsecretmessagesecretsecretmessagesecretsecret")
+        XCTAssertEqual(messageDecryptor.decryptMessage("3[message2[secret]]"), "messagesecretsecretmessagesecretsecretmessagesecretsecret")
     }
     
     func test9() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("2[2[secret]]message"), "secretsecretsecretsecretmessage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("2[2[secret]]message"), "secretsecretsecretsecretmessage")
     }
     
     func test10() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("1[s]1[e]1[c]1[r]1[e]1[t]1[m]1[e]2[s]1[a]1[g]1[e]"), "secretmessage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("1[s]1[e]1[c]1[r]1[e]1[t]1[m]1[e]2[s]1[a]1[g]1[e]"), "secretmessage")
     }
     
     func test11() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("1[s]2[e]3[c]4[r]5[e10[t]]11[m]12[e13[s]]3[a]1[g]1[e]"), "seecccrrrrettttttttttettttttttttettttttttttettttttttttettttttttttmmmmmmmmmmmesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssaaage")
+        XCTAssertEqual(messageDecryptor.decryptMessage("1[s]2[e]3[c]4[r]5[e10[t]]11[m]12[e13[s]]3[a]1[g]1[e]"), "seecccrrrrettttttttttettttttttttettttttttttettttttttttettttttttttmmmmmmmmmmmesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssesssssssssssssaaage")
     }
     
     func test12() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("2[s2[e2[c2[r2[e2[t2[m]]]]]]essage]"),
+        XCTAssertEqual(messageDecryptor.decryptMessage("2[s2[e2[c2[r2[e2[t2[m]]]]]]essage]"),
                        "secretmmtmmetmmtmmretmmtmmetmmtmmcretmmtmmetmmtmmretmmtmmetmmtmmecretmmtmmetmmtmmretmmtmmetmmtmmcretmmtmmetmmtmmretmmtmmetmmtmmessagesecretmmtmmetmmtmmretmmtmmetmmtmmcretmmtmmetmmtmmretmmtmmetmmtmmecretmmtmmetmmtmmretmmtmmetmmtmmcretmmtmmetmmtmmretmmtmmetmmtmmessage")
     }
     
     func test13() {
-        XCTAssertEqual(messageDecryptor.getDecryptMessage("[s6[e5[c4[r3[e2[t1[m]e]s]s]a]g]e]"),
+        XCTAssertEqual(messageDecryptor.decryptMessage("[s6[e5[c4[r3[e2[t1[m]e]s]s]a]g]e]"),
                        "secretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessagecretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessagecretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessagecretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessagecretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessagecretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessacretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessretmetmesetmetmesetmetmessage")
     }
 }
