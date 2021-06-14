39e4a0a8cd600a3fc3cc8740013b81cfe75e3304
BOTTAK <djbottak@gmail.com>
2021-06-14 10:55:21 +0300

    Homework is Done

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -2,7 +2,38 @@ import Foundation
 
 class StockMaximize {
 
+    func getAllCount(prices: [Int]) -> Int {
+        var profit = 0
+        for price in prices {
+            profit += prices.last! - price
+        }
+        return profit
+    }
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        guard prices.count > 1 else {
+            return 0
+        }
+        
+        var profit = 0
+        var somePrices = [Int]()
+        somePrices.append(prices.first!)
+        
+        for index in 0...(prices.count-2) {
+            if prices[index] <= prices[index+1] {
+                somePrices.append(prices[index+1])
+            } else {
+                if somePrices.count > 1 {
+                    profit += getAllCount(prices: somePrices)
+                }
+                somePrices.removeAll()
+                somePrices.append(prices[index+1])
+                
+            }
+        }
+        if somePrices.count > 1 {
+            profit += getAllCount(prices: somePrices)
+        }
+        return profit
     }
 }
